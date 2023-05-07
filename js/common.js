e3547f9eece851214495e43687cf226ae3f74405
m <131961031+dev-m-tanasov@users.noreply.github.com>
2023-05-07 18:34:51 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(function () {
+
+    // Parallax background
+    // http://markdalgleish.com/projects/stellar.js/docs/
+    $.stellar({
+        responsive: false,
+        horizontalScrolling: false, // обязательно, чтобы не было горизонтального сдвига
+    });
+
+});
+
+//# sourceMappingURL=common.js.map
---
+++ js/main.js
@@ -0,0 +1,19 @@
+///////////  PARALAX  ///////////////
+
+// $.stellar();
+
+
+/////////////// SLIDER ///////////////
+
+// $('.wrapper').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     dots: false,
+//     autoplay:true,
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     cssEase: 'linear',
+//     adaptiveHeight: true
+//   });
+  
